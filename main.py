c0742ce5b3f3f50a364a3946f3a4f7b0997f679b
cwavedave <david@creativewavelength.co.uk>
2021-03-20 22:40:14 +0100

    Web Scrapped succesfully

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-bb_search = input("Find out what the top 100 songs were the year you were born! /n Write your birthdate in this format YYYY-MM-DD")
+bb_search = input("Find out what the top 100 songs were the year you were born! \nWrite your birthdate in this format YYYY-MM-DD\n")
 
 response = requests.get(f"https://www.billboard.com/charts/hot-100/{bb_search}")
-yc_website = response.text
+bb_search = response.text
 
-soup = BeautifulSoup(yc_website, "html.parser")
-td_data = soup.findAll(name="a", class_="storylink")
+soup = BeautifulSoup(bb_search, "html.parser")
+song_data = soup.findAll(name="span", class_="chart-element__information__song")
+
+songs = []
+
+for song in song_data:
+    song = song.getText()
+    songs.append(song)
+
+artists = [artist.getText() for artist in soup.findAll(name="span", class_="chart-element__information__artist")]
+
+print(songs)
+print(artists)
